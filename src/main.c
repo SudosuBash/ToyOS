6b89238ec8cf7f9c3a028a46fcd80d41786493d4
Sudo-su-Bash <AlienBash@outlook.com>
2026-03-12 12:30:42 +0000

    update .gitignore

--- src/main.c
+++ src/main.c
@@ -10,22 +10,7 @@ void kernel_start() {
     init_cpu();
     init_irq();
     init_mm();
-
-    //测试 SLUB 分配器
-    // struct kmem_cache* cache = kmem_cache_get(4096);
-    // struct kmem_cache* cache2 = kmem_cache_get(4096);
-    // void* addr = kmem_cache_alloc(cache);
-    // kmem_cache_free(addr);
-    // kmem_cache_free(cache);
-    // kmem_cache_free(cache2);
-    // struct page* c = alloc_page(3);
-    // struct page* c2 = alloc_page(3);
-    // struct page* c3 = alloc_page(3);
-    // free_page(c3);
     
-    // free_page(c);
-    // free_page(c2);
-
     void* p = kmalloc(8);
     kfree(p);
     p=0;
